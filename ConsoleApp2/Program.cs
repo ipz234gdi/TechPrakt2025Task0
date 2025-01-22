086a8017f76db658c06b6ac405ad480df1ab739f
Reiclid <reiclid@gmail.com>
2025-01-22 14:23:57 +0200

    add C# app and .gitignore

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введіть перше число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введіть друге число: ");
+int b = int.Parse(Console.ReadLine());
+
+int r = a + b;
+
+Console.WriteLine($"Результат додавання: {r}");
